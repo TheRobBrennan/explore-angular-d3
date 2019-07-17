880e26049f3ec037fb70aaa16323e65388dbb1dd
Rob Brennan <rob@therobbrennan.com>
2019-07-17 12:27:45 -0700

    Now loading data in app component

--- examples/responsive-bar-graph/src/app/app.component.ts
+++ examples/responsive-bar-graph/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { DataModel } from 'src/app/data/data.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'responsive-bar-graph';
+  data: Observable<DataModel>;
+
+  constructor(private http: HttpClient) {
+    this.data = this.http.get<DataModel>('./assets/data.json');
+  }
 }
