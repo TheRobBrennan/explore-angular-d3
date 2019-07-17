c20e005df270d107eecf3d5a59655683942c3574
Rob Brennan <rob@therobbrennan.com>
2019-07-17 14:24:41 -0700

    Added comments for generating nodes and links

--- examples/positive-integers-connected-to-their-dividers/src/app/app.component.ts
+++ examples/positive-integers-connected-to-their-dividers/src/app/app.component.ts
@@ -21,6 +21,24 @@ export class AppComponent {
       this.nodes.push(new Node(i));
     }
 
+    /**
+     * Here is where the magic is. Let's say we have N defined as 3.
+     *
+     * Our processing loop would look something like:
+     *
+     * 1
+     *  1 * 2 <= 3 :: Increment link count on the node containing 1 and the node containing 2 and create a Link
+     *  1 * 3 <= 3 :: Increment link count on the node containing 1 and the node containing 3 and create a Link
+     *
+     * 2
+     *  2 * 2 > 3 therefore we will not increment link counts or create a link
+     *
+     * 3
+     *  3 * 2 > 3 therefore we will not increment link counts or create a link
+     *
+     * So, what happens if we have N defined as 6? Following the same processing loop above, we see that
+     * the number 2 is connected to 1, 4, and 6.
+     */
     for (let i = 1; i <= N; i++) {
       for (let m = 2; i * m <= N; m++) {
         /** increasing connections toll on connecting nodes */
